0232d16bd7e804c9971427226c5b37f40b0ed679
zinedkaloc <deniz@altogic.com>
2023-06-09 16:05:29 +0300

    Remove contact page and update typo

--- app/page.tsx
+++ app/page.tsx
@@ -436,8 +436,7 @@ export default function IndexPage() {
       <section className="space-y-6 bg-slate-50 py-8 pt-6 dark:bg-transparent md:pb-12 md:pt-10 lg:py-32">
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
           <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-            Fully-customizable Development
-            <br className="hidden sm:inline" />
+            Fully-customizable Development <br className="hidden sm:inline" />{" "}
             Environment in Minutes
           </h1>
           <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl"></h1>
